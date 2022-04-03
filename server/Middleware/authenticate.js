02976f3928a180d25f92605ada632ccb2496820d
nagasaisriya <63009472+nagasaisriya@users.noreply.github.com>
2022-04-03 18:33:35 +0530

    add middleware

---
+++ server/Middleware/authenticate.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import  User from '../model/userSchema.js';
+
+const Authenticate = async (req,res,next) => {
+    try{
+        const token = req.cookies.jwtoken;
+        const verifyToken = jwt.verify(token,process.env.SECRET_KEY);
+
+        const rootUser = await User.findOne({_id: verifyToken._id,"tokens.token":token});
+        if(!rootUser)
+        {
+            throw new Error("User not found");
+        }
+        req.token = token;
+        req.rootUser = rootUser;
+        req.userId=rootUser._id;
+
+        next();
+
+    }catch(err){
+        res.status(401).send("Unauthorized: No token provided");
+        console.log(err);
+
+    }
+
+}
+export default Authenticate;
